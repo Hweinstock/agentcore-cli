f71c191e46a2d738b540f5d35176526a2d4ac147
Hweinstock <hkobew@amazom.com>
2026-08-27 03:13:19 +0000

    fix(scaffold): allow certain template values to be overriden

--- src/handlers/project/types.ts
+++ src/handlers/project/types.ts
@@ -10,44 +10,6 @@ import { AgentNameSchema, BuildTypeSchema, EntrypointSchema } from "../../projec
 import { RuntimeVersionSchema } from "../../projectSchemas/constants";
 import type { AgentCoreGateway, AgentCoreGatewayTarget } from "../../projectSchemas/gateway";
 
-export const RUNTIME_TEMPLATE_SHORTCUTS = {
-  "hello-world-python": {
-    runtimeName: "hello_world",
-    build: "CodeZip",
-    language: "Python",
-    framework: "none",
-    modelProvider: "Bedrock",
-    memory: "none",
-    entrypoint: "main.py",
-    runtimeVersion: "PYTHON_3_14",
-  },
-  "hello-world-python-container": {
-    runtimeName: "hello_world",
-    build: "Container",
-    language: "Python",
-    framework: "none",
-    modelProvider: "Bedrock",
-    memory: "none",
-    entrypoint: "main.py",
-  },
-  "strands-python": {
-    runtimeName: "strands_agent",
-    build: "CodeZip",
-    language: "Python",
-    framework: "strands",
-    modelProvider: "Bedrock",
-    memory: "none",
-    entrypoint: "main.py",
-    runtimeVersion: "PYTHON_3_14",
-  },
-} as const satisfies Record<string, ScaffoldRuntimeInput>;
-
-export type RuntimeTemplateShortcutName = keyof typeof RUNTIME_TEMPLATE_SHORTCUTS;
-
-export const RUNTIME_TEMPLATE_SHORTCUT_NAMES = Object.keys(
-  RUNTIME_TEMPLATE_SHORTCUTS,
-) as unknown as readonly [RuntimeTemplateShortcutName, ...RuntimeTemplateShortcutName[]];
-
 type CreateProjectInputBase = {
   /** The name of the project; also the directory it is scaffolded into. */
   name: string;
