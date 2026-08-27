ca4eaa80ac4d3003d646ef4f79a738d4b2692d33
Hweinstock <hkobew@amazom.com>
2026-08-27 01:24:25 +0000

    fix(templates): add harnesses to merge entries

--- src/core/project/templates/project.ts
+++ src/core/project/templates/project.ts
@@ -62,10 +62,12 @@ function mergeSpecEntries(entries: SpecEntries[]): SpecEntries {
   const runtimes = entries.flatMap(({ runtimes }) => runtimes ?? []);
   const credentials = entries.flatMap(({ credentials }) => credentials ?? []);
   const memories = entries.flatMap(({ memories }) => memories ?? []);
+  const harnesses = entries.flatMap(({ harnesses }) => harnesses ?? []);
 
   return {
     ...(runtimes.length > 0 && { runtimes }),
     ...(credentials.length > 0 && { credentials }),
     ...(memories.length > 0 && { memories }),
+    ...(harnesses.length > 0 && { harnesses }),
   };
 }
