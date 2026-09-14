63112fce15a3042df407e8d5ae2b5ce35a474758
Nicolas <nborges.aws@gmail.com>
2026-09-14 17:02:22 -0400

    feat: support project aware logging in runtime (#2271)
    * feat: support project aware logging in runtime
    
    * chore: update ReadMe with project observability
    
    * chore: update ReadMe language for clarity
    
    * fix: use initProject helper for test scaffolding
    
    * chore: inline log group resolution in handler

--- src/core/observability/index.ts
+++ src/core/observability/index.ts
@@ -1,5 +1,10 @@
 export { CloudWatchClient } from "./cloudWatchClient";
 export { ObservabilityClient } from "./client";
+export const DEFAULT_ENDPOINT_QUALIFIER = "DEFAULT";
+
+export function runtimeLogGroup(runtimeId: string, endpoint: string): string {
+  return `/aws/bedrock-agentcore/runtimes/${runtimeId}-${endpoint}`;
+}
 export {
   INSIGHTS_MAX_ROWS,
   runInsightsQuery,
@@ -9,6 +14,7 @@ export {
 export { TRACE_RECORD_LIMIT } from "./traces";
 export type {
   CloudWatchLogEvent,
+  CoreObservabilityClient,
   GetTraceQuery,
   InsightsQuery,
   InsightsQueryRow,
